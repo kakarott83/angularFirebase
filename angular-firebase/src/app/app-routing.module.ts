6652babfd5172f16665502a25151ec0c959561d9
kakarott83 <lm28081983@gmail.com>
2020-03-14 17:51:23 +0100

    signin,out

--- angular-firebase/src/app/app-routing.module.ts
+++ angular-firebase/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LogoutComponent } from './logout/logout.component';
+import { TravelComponent } from './travel/travel.component';
+import { SettingsComponent } from './settings/settings.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'logout', component: LogoutComponent},
+  {path: 'travel', component: TravelComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: '**', component: LoginComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-firebase/src/app/app.module.ts
+++ angular-firebase/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
@@ -17,13 +18,26 @@ import { TravelComponent } from './travel/travel.component';
 import { TravelListComponent } from './travel-list/travel-list.component';
 import { TravelService } from './shared/travel.service';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LogoutComponent } from './logout/logout.component';
+import { CustomerComponent } from './customer/customer.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ReasonComponent } from './reason/reason.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TravelComponent,
     TravelListComponent,
-    NavBarComponent
+    NavBarComponent,
+    LoginComponent,
+    RegisterComponent,
+    LogoutComponent,
+    
+    CustomerComponent,
+    SettingsComponent,
+    ReasonComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +47,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     ToastrModule.forRoot(),
     AngularFireModule.initializeApp(environment.fireBaseConfig),
     AngularFirestoreModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    NgbModule
   ],
   providers: [TravelService],
   bootstrap: [AppComponent]
